0e1854b47d2fdc1df72e05b06a869eca4a4f21ec
AjayRaja1116 <ajayraja1116@gmail.com>
2026-09-08 08:31:02 +0530

    Time: 4 ms (98.13%) | Memory: 47.2 MB (17.64%) - LeetSync

---
+++ 4280-count-digit-appearances/count-digit-appearances.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countDigitOccurrences(int[] nums, int digit) {
+        int count=0;
+        for(int num:nums)
+        {
+            while(num>0)
+            {
+                if(num%10==digit)
+                {
+                    count++;
+                }
+                num=num/10;
+            }
+        }
+        return count;
+    }
+}
