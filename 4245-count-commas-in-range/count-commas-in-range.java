f99244635bbe03fbd4622eb1ba2b8c0406ac0266
AjayRaja1116 <ajayraja1116@gmail.com>
2026-09-08 08:16:47 +0530

    Time: 1 ms (83.65%) | Memory: 42.5 MB (72.85%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -4,11 +4,6 @@ class Solution {
         {
             return 0;
         }
-        int res=0;
-        for(int i=1000;i<=n;i++)
-        {
-            res++;
-        }
-        return res;
+        return n-999;
     }
 }
