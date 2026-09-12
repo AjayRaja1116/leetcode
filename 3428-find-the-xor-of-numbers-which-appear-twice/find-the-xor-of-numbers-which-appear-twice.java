0fdf54a64d6af46718ac0aea6c84eb3ba138aa59
AjayRaja1116 <ajayraja1116@gmail.com>
2026-09-12 20:29:19 +0530

    Time: 2 ms (63.06%) | Memory: 45.2 MB (21.34%) - LeetSync

--- 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.java
+++ 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.java
@@ -2,16 +2,16 @@ import java.util.*;
 class Solution {
     public int duplicateNumbersXOR(int[] nums) {
         HashMap<Integer,Integer> map=new HashMap<>();
+        int prod=0;
         for(int num:nums)
         {
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        int prod=0;
-        for(int num:map.keySet())
-        {
-            if(map.get(num)==2)
+            if(map.containsKey(num))
             {
                 prod^=num;
+            }
+            else
+            {
+                map.put(num,0);
             }
         }
         return prod;
