13d27f2e5e20b6ab0fef967740f3398416bb7d81
AjayRaja1116 <ajayraja1116@gmail.com>
2026-09-15 20:39:23 +0530

    Time: 2 ms (99.64%) | Memory: 45.1 MB (51.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,30 +1,15 @@
-import java.util.*;
-
 class Solution {
     public boolean isAnagram(String s, String t) {
-
-        if (s.length() != t.length()) {
-            return false;
+        if(s.length()!=t.length()) return false;
+        int[] freq = new int[128];
+        for(char ch: s.toCharArray()) freq[ch]++;
+        for(char ch: t.toCharArray()) freq[ch]--;
+        for(char ch: s.toCharArray()){
+            if(freq[ch]!=0) return false;
         }
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        for (char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-
-        for (char c : t.toCharArray()) {
-            if (!map.containsKey(c)) {
-                return false;
-            }
-
-            map.put(c, map.get(c) - 1);
-
-            if (map.get(c) == 0) {
-                map.remove(c);
-            }
-        }
-
-        return map.isEmpty();
+        return true;
     }
 }
+
+// s = "car"
+// t = "rac"
